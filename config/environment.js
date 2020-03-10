2a22c8b025c573fcaf6c50e28a6050b22aace546
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-03-10 09:27:38 -0400

    working on my thing

--- config/environment.js
+++ config/environment.js
@@ -13,8 +13,8 @@ module.exports = {
     host: 'localhost',
     mysqlPort: '3306',
     mysqlUser: 'root',
-    mysqlPass: "MiNegrita47",
-    //		mysqlPass: "mysqltecnobunker",
+//    mysqlPass: "MiNegrita47",
+    		mysqlPass: "mysqltecnobunker",
     dbName: 'tampaDb',
     connectionLimit: 100
 
